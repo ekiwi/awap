c24978e98e07256f033a7fecf725332f64fa2a66
Kevin Laeufer <kevin.laeufer@rwth-aachen.de>
2015-06-29 15:53:20 +0200

    [agents] add simple TimeoutAgent

--- agents/timeout/TimeoutAgent.java
+++ agents/timeout/TimeoutAgent.java
@@ -1,23 +1,22 @@
 package de.rwth_aachen.awap.agents;
 
 import de.rwth_aachen.awap.Agent;
-import de.rwth_aachen.awap.EventHandler;
+import de.rwth_aachen.awap.Node;
 
-public class TimeoutAgent implements EventHandler{
-	private String name;
-	private Agent agent;
+public class TimeoutAgent implements Agent{
 
-	public TimeoutAgent(String name, Agent agent) {
+	public TimeoutAgent(String name, Node node) {
+		
 		this.name = name;
-		this.agent = agent;
-		this.agent.registerTimeout(this, 1000);
-		this.agent.registerTimeout(this, 2000);
+		this.node = agent;
+		this.node.registerTimeout(this, 1000);
+		this.node.registerTimeout(this, 2000);
 	}
 
 	public void handleEvent(Event event) {
 		if(event instanceof TimeoutEvent) {
 			int deltaTimeMs = ((TimeoutEvent)event).deltaTimeMs;
-			System.out.println(Agent.this.name + ": Timeout called after " + deltaTimeMs + " ms.");
+			System.out.println(this.name + ": Timeout called after " + deltaTimeMs + " ms.");
 		}
 	}
 
