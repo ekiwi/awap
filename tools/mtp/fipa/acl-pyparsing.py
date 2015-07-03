0bd6e9378347fb50605e90fbd61a9700f2cbd89e
Electronic Kiwi <electron.kiwi@gmail.com>
2015-07-03 17:05:08 +0200

    [tools] mtp: start working on a pyparsing based ACL parser

---
+++ tools/mtp/fipa/acl-pyparsing.py
@@ -0,0 +1,177 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import unittest
+import datetime
+from pyparsing import Regex, ParseException, Or, Literal, Optional, ZeroOrMore, Suppress
+
+
+
+def parse_DateTime(source, location, tokens):
+	tok = tokens[0]
+	# ignore sign for now
+	if tok[:1] in ['+', '-']:
+		tok = tok[1:]
+	ts = datetime.datetime(
+		year = int(tok[0:4]),
+		month = int(tok[4:6]),
+		day = int(tok[6:8]),
+		hour = int(tok[9:11]),
+		minute = int(tok[11:13]),
+		second = int(tok[13:15]),
+		microsecond = int(tok[15:18]) * 1000)
+	return ts
+
+def parse_StringLiteral(source, location, tokens):
+	tok = tokens[0][1:-1]
+	tok = tok.replace('\\"', '"')
+	return tok
+
+def parse_Integer(source, location, tokens):
+	return int(tokens[0])
+
+DateTime = Regex(r'([\+-])?\d{8}T\d{9}([a-zA-Z])?')
+DateTime.setParseAction(parse_DateTime)
+
+StringLiteral = Regex(r'"(([^"])|(\"))+"')
+StringLiteral.setParseAction(parse_StringLiteral)
+
+# FIPA ACL allows Word to start with " or +, we don't in order to make
+# it possible to destinguish Integer and StringLiteral from Word
+Word = Regex(r'\s*[^\(\)#0-9-@ "+][^\(\) ]*')
+
+Integer = Regex(r'[\+-]?[0-9]+')
+Integer.setParseAction(parse_Integer)
+
+Number = Integer
+
+String = StringLiteral
+
+# inspired by: https://stackoverflow.com/questions/6883049/regex-to-find-urls-in-string-in-python
+# TODO: this is not correct, as any URL, not only http urls should be accepted...
+URL = Regex(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-&*-_@.&+]|[!*,]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
+
+URLSequence = Suppress("(") + Suppress("sequence") + ZeroOrMore(URL) + Suppress(")")
+
+AgentIdentifier = Suppress("(") + Suppress("agent-identifier") + Suppress(":name") + Word + Optional(Suppress(":addresses") + URLSequence) + Suppress(")")
+
+Expression = Or([Word, String, Number, DateTime])
+
+class TestACLStringParser(unittest.TestCase):
+
+	def test_DateTime(self):
+		ts = datetime.datetime(2015, 7, 1, 14, 39, 41, 567 * 1000)
+		self.assertEqual(DateTime.parseString("20150701T143941567")[0], ts)
+		self.assertEqual(DateTime.parseString("+20150701T143941567")[0], ts)
+		self.assertEqual(DateTime.parseString("-20150701T143941567z")[0], ts)
+		self.assertRaises(ParseException, DateTime.parseString, "20150701Z143941567")
+		self.assertRaises(ParseException, DateTime.parseString, "d20150701T143941567")
+		self.assertRaises(ParseException, DateTime.parseString, "20150d01T143941567")
+		self.assertRaises(ParseException, DateTime.parseString, "20150701T14394156")
+
+	def test_StringLiteral(self):
+		self.assertEqual(StringLiteral.parseString('"String"')[0], 'String')
+		self.assertEqual(StringLiteral.parseString('"str\\"ing"')[0], 'str"ing')
+		self.assertEqual(StringLiteral.parseString('"String"not part of this String')[0], 'String')
+		self.assertRaises(ParseException, StringLiteral.parseString, 'not a String literal')
+
+	def test_Word(self):
+		self.assertEqual(Word.parseString('Word')[0], 'Word')
+		self.assertEqual(Word.parseString('WoRd')[0], 'WoRd')
+		self.assertEqual(Word.parseString('w#ord')[0], 'w#ord')
+		self.assertEqual(Word.parseString('w0rd')[0], 'w0rd')
+		self.assertEqual(Word.parseString('w-r+d')[0], 'w-r+d')
+		self.assertEqual(Word.parseString('rma@192.168.122.1:1099/JADE')[0], 'rma@192.168.122.1:1099/JADE')
+		self.assertRaises(ParseException, Word.parseString, '#notaWord')
+
+	def test_Integer(self):
+		self.assertEqual(Integer.parseString('+100')[0], 100)
+		self.assertEqual(Integer.parseString('-100')[0], -100)
+
+	@unittest.expectedFailure
+	def test_String(self):
+		# StringLiteral
+		self.assertEqual(String.parseString('"String"')[0], 'String')
+		# ByteLengthEncodedString (not implemented yet => fails)
+		self.assertEqual(String.parseString('#6"String')[0], 'String')
+
+	def test_Expression(self):
+		ts = datetime.datetime(2015, 7, 1, 14, 39, 41, 567 * 1000)
+		self.assertEqual(Expression.parseString("20150701T143941567")[0], ts)
+		self.assertEqual(Expression.parseString('"String"')[0], 'String')
+		self.assertEqual(Expression.parseString('w#ord')[0], 'w#ord')
+		self.assertEqual(Expression.parseString('+100')[0], 100)
+
+	def test_URL(self):
+		self.assertEqual(URL.parseString('http://test/')[0], 'http://test/')
+		self.assertEqual(URL.parseString('http://1')[0], 'http://1')
+		self.assertEqual(URL.parseString(
+			'http://ip2-127.halifax.rwth-aachen.de:7778/acc')[0],
+			'http://ip2-127.halifax.rwth-aachen.de:7778/acc')
+		self.assertEqual(URL.parseString(
+			'http://130-000.eduroam.rwth-aachen.de:7778/acc')[0],
+			'http://130-000.eduroam.rwth-aachen.de:7778/acc')
+		# even if this might rule out some legitimate urls, it is important,
+		# that the closing bracket is NOT matched
+		# this behavior is needed in order for the URLSequence to work
+		self.assertEqual(URL.parseString('http://1)')[0], 'http://1')
+
+	def test_URLSequence(self):
+		s0 = "(sequence http://1 http://2 http://3)"
+		s1 = "( sequence http://1 http://2 http://3 )"
+		s2 = "(  sequence  http://1   http://2 http://3)"
+		s3 = "(  sequence  http://1   http://2)"
+		s4 = "(sequence http://130-000.eduroam.rwth-aachen.de:7778/acc )"
+		res = ['http://1', 'http://2', 'http://3']
+		self.assertEqual(list(URLSequence.parseString(s0)), res)
+		self.assertEqual(list(URLSequence.parseString(s1)), res)
+		self.assertEqual(list(URLSequence.parseString(s2)), res)
+		self.assertEqual(list(URLSequence.parseString(s3)), ['http://1', 'http://2'])
+		self.assertEqual(list(URLSequence.parseString(s4)), ['http://130-000.eduroam.rwth-aachen.de:7778/acc'])
+
+	def test_AgentIdentifier(self):
+		a0 = "( agent-identifier :name rma@192.168.122.1:1099/JADE  :addresses (sequence http://130-000.eduroam.rwth-aachen.de:7778/acc ) )"
+		a1 = "( agent-identifier :name test  :addresses (sequence http://1 http://2 ) )"
+		self.assertEqual(list(AgentIdentifier.parseString(a0)), ['rma@192.168.122.1:1099/JADE', 'http://130-000.eduroam.rwth-aachen.de:7778/acc'])
+		self.assertEqual(list(AgentIdentifier.parseString(a1)), ['test', 'http://1', 'http://2'])
+
+
+
+
+
+if __name__ == "__main__":
+	unittest.main()
+#	year   = pyparsing.Regex(r'\d{4}')
+#	month  = pyparsing.Regex(r'\d{2}')
+#	day    = pyparsing.Regex(r'\d{2}')
+#	hour   = pyparsing.Regex(r'\d{2}')
+#	minute = pyparsing.Regex(r'\d{2}')
+#	second = pyparsing.Regex(r'\d{2}')
+#	millisecond = pyparsing.Regex(r'\d{3}')
+#	type_designator = pyparsing.Regex(r'[a-zA-Z]')
+#	sign = pyparsing.Regex(r'[\+-]')
+#	data_time_token = pyparsing.Optional(sign) + year + month + day + "T" + hour + minute + second + millisecond
+
+
+
+
+
+
+
+#	msg = """
+
+
+#(REQUEST
+# :language  fipa-sl0  :ontology  FIPA-Agent-Management  :protocol  fipa-request
+# :sender  ( agent-identifier :name rma@192.168.122.1:1099/JADE  :addresses (sequence http://130-000.eduroam.rwth-aachen.de:7778/acc ))
+# :receiver  (set ( agent-identifier :name test  :addresses (sequence http://localhost:9000 )) )
+# :content  "((action (agent-identifier :name test :addresses (sequence http://localhost:9000)) (get-description)))" 
+# :language  fipa-sl0  :ontology  FIPA-Agent-Management  :protocol  fipa-request
+# :conversation-id  C1438784720_1435754381566 )
+
+#"""
+#	print("-----------------------------")
+#	print("Testing from_mtp")
+
+#	acl_msg = ACLMessage.from_mtp(envelope, msg)
+#	print(acl_msg)
