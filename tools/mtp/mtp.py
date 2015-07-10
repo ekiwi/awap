2d72f52846a1ebe60bff1e6e4214ee688f88bc6d
Kevin Laeufer <kevin.laeufer@rwth-aachen.de>
2015-07-10 15:45:55 +0200

    [mtp] can receive and decode acl messages

--- tools/mtp/mtp.py
+++ tools/mtp/mtp.py
@@ -25,7 +25,7 @@ Content-Type: {}
 """
 
 class Mtp(object):
-	def __init__(self, url):
+	def __init__(self, addresse, url):
 		self.url = url
 		self.boundary = "a1723f6311bdcdc73e4145537302b33"
 
@@ -33,20 +33,29 @@ class Mtp(object):
 			"Content-type": "multipart/mixed ; boundary=\"{}\"".format(self.boundary),
 			"Connection": "keep-alive"}
 		self.tx = http.client.HTTPConnection(self.url)
-		self.tx_msgs = queue.Queue()
+		self.tx_queue = queue.Queue()
 		self.tx_thread = threading.Thread(target=self.sender)
 		# self.tx_thread.setDaemon(True)
 		self.tx_thread.start()
 
+		self.rx_queue = queue.Queue()
+		self.rx_server = MTPServer(addresse, self.rx_queue)
+		self.rx_thread = threading.Thread(target=self.receiver)
+		# self.rx_thread.setDaemon(True)
+		self.rx_thread.start()
+
 	def send(self, msg):
 		if isinstance(msg, ACLMessage):
-			self.tx_msgs.put(msg)
+			self.tx_queue.put(msg)
+
+	def receive(self):
+		return self.rx_queue.get()
 
 	def sender(self):
 		""" Thread that is responsible for sending ACLMessages
 		"""
 		while True:
-			msg = self.tx_msgs.get()
+			msg = self.tx_queue.get()
 			if msg is None:
 				break
 			body = msg_frmt.format(msg.generateEnvelope(), msg.mime_type, msg)
@@ -55,17 +64,16 @@ class Mtp(object):
 			response = self.tx.getresponse()
 			if response.reason != "OK":
 				print("Error: failed to send message")
-			self.tx_msgs.task_done()
+			self.tx_queue.task_done()
 		self.tx.close()
 
 	def receiver(self):
 		""" Thread that is responsible for receiving ACLMessages
 		"""
-		pass
+		self.rx_server.serve_forever()
 
 
-class CustomHTTPHandler(BaseHTTPRequestHandler):
-
+class MTPHandler(BaseHTTPRequestHandler):
 	def do_POST(self):
 		length = int(self.headers.get('content-length'))
 		ct = self.headers.get('content-type')
@@ -80,10 +88,8 @@ class CustomHTTPHandler(BaseHTTPRequestHandler):
 			print("Error: did not receive the correct mime type")
 			self.send_error(406)	# TODO: what is the correct code?
 		else:
-			print("Envelope:")
-			print(envelope)
-			print("Message")
-			print(message)
+			msg = ACLMessage.from_mtp(envelope, message)
+			self.server.rx_queue.put(msg)
 			self.send_ok()
 
 	def send_ok(self):
@@ -110,30 +116,26 @@ class CustomHTTPHandler(BaseHTTPRequestHandler):
 		print(self.headers)
 
 
+class MTPServer(HTTPServer):
+	def __init__(self, addresse, rx_queue):
+		self.rx_queue = rx_queue
+		super().__init__(addresse, MTPHandler)
 
 if __name__ == "__main__":
-#	boundary = "a1723f6311bdcdc73e4145537302b33"
-
-	acl_msg = ACLMessage(Performative.INFORM)
-	acl_msg.sender     = AgentIdentifier("ams@192.168.122.1:5000/JADE", "http://ip2-127.halifax.rwth-aachen.de:57727/acc")
-	acl_msg.receivers += [AgentIdentifier("ams@192.168.122.1:1099/JADE", "http://ip2-127.halifax.rwth-aachen.de:7778/acc")]
-	acl_msg.content = '"((result (action (agent-identifier :name ams@192.168.122.1:5000/JADE :addresses (sequence http://ip2-127.halifax.rwth-aachen.de:5000/acc)) (get-description)) (sequence (ap-description :name 192.168.122.1:5000/JADE :ap-services (sequence (ap-service :name fipa.mts.mtp.http.std :type fipa.mts.mtp.http.std :addresses (sequence http://ip2-127.halifax.rwth-aachen.de:5000/acc)))))))"'
-	acl_msg.reply_with = "rma@192.168.122.1:1099/JADE1435662093800"
-	acl_msg.language = "fipa-sl0"
-	acl_msg.ontology = "FIPA-Agent-Management"
-	acl_msg.protocol = "fipa-request"
-
 #	mtp = Mtp("130-000.eduroam.rwth-aachen.de:7778")
 #	mtp.send(acl_msg)
 
 #	# TODO: implement in Mtp method
-#	mtp.tx_msgs.join()
+#	mtp.tx_queue.join()
 #	print("done")
-#	mtp.tx_msgs.put(None)
+#	mtp.tx_queue.put(None)
 
 
-	httpd = HTTPServer(('', 9000), CustomHTTPHandler)
-	httpd.serve_forever()
+	mtp = Mtp(('', 9000), "130-000.eduroam.rwth-aachen.de:7778")
+
+	while True:
+		msg = mtp.receive()
+		print(msg)
 
 
 
