420b37f607cf2db6be739e0b332420a21ad536cb
Kevin Laeufer <kevin.laeufer@rwth-aachen.de>
2015-07-12 14:55:11 +0200

    [mtp] fasterparsing: enable parse_actions for Or and And

--- tools/mtp/fipa/fasterparsing.py
+++ tools/mtp/fipa/fasterparsing.py
@@ -17,19 +17,24 @@ class ParseException(Exception):
 class Element(object):
 	def __init__(self):
 		self.endpos = 0
-		self.parse_action = None
+		self.parse_actions = [self._defaultParseAction]
 
 	def getRegexString(self):
 		return None
 
 	def setParseAction(self, callback):
-		self.parse_action = callback
+		self.parse_actions = [callback]
+
+	def _defaultParseAction(self, source, pos, tockens):
+		return tockens
 
 class RawRegex(Element):
-	def __init__(self, string):
+	def __init__(self, string, parse_actions=None):
 		super().__init__()
 		self._compiled_re = None
 		self._string = string
+		if parse_actions:
+			self.parse_actions = parse_actions
 
 	def getRegexString(self):
 		return self._string
@@ -41,11 +46,16 @@ class RawRegex(Element):
 		if m:
 			self.endpos = m.endpos
 			res_list = list(m.groups())
-			res = [rr for rr in res_list if rr]
-			if self.parse_action:
-				new_res = [self.parse_action(string, pos, res)]
-				res = new_res if new_res else res
-			return res
+			if len(res_list) != len(self.parse_actions):
+				# this is NOT a ParseException, since it should never happen
+				raise Exception("Error: number of results ({}) is ".format(len(res_list)) +
+					"not equal to the number of parse_actions ({})".format(len(self.parse_actions)))
+			results = []
+			for (result, parse_action) in zip(res_list, self.parse_actions):
+				if result:
+					parsed = parse_action(string, pos, result)
+					results.append(parsed if parsed else result)
+			return results
 		else:
 			raise ParseException("Failed to find `{}` @:\n`{}`".format(self.getRegexString(), string[pos:]))
 
@@ -53,6 +63,8 @@ class Regex(RawRegex):
 	def __init__(self, string, suppress=False):
 		super().__init__(string)
 		self._suppress = suppress
+		if suppress:
+			self.parse_actions = []
 
 	def getRegexString(self):
 		if self._suppress:
@@ -99,19 +111,21 @@ class And(MultiElement):
 	def compressElements(self, elements):
 		compressed = []
 		re_string = ""
+		re_parse_actions = []
 		for ee in elements:
 			if isinstance(ee, str):
 				ee = Regex(ee)
 			ee_re = ee.getRegexString()
 			if ee_re:
 				re_string += ee_re
+				re_parse_actions += ee.parse_actions
 			else:
 				if len(re_string) > 0:
-					compressed.append(RawRegex(re_string))
+					compressed.append(RawRegex(re_string, re_parse_actions))
 					re_string = ""
 				compressed.append(ee)
 		if len(re_string) > 0:
-			compressed.append(RawRegex(re_string))
+			compressed.append(RawRegex(re_string, re_parse_actions))
 		return compressed
 
 	def parseString(self, string, pos=0):
@@ -134,18 +148,20 @@ class Or(MultiElement):
 	def compressElements(self, elements):
 		compressed = []
 		re_strings = []
+		re_parse_actions = []
 		for ee in elements:
 			if isinstance(ee, str):
 				ee = Regex(ee)
 			ee_re = ee.getRegexString()
 			if ee_re:
 				re_strings.append(ee_re)
+				re_parse_actions += ee.parse_actions
 			else:
 				compressed.append(ee)
 		if len(re_strings) > 0:
 			# build or'd regex string and insert at beginning of the list
 			re_string = '|'.join(re_strings)
-			compressed.insert(0, RawRegex(re_string))
+			compressed.insert(0, RawRegex(re_string, re_parse_actions))
 		return compressed
 
 	def parseString(self, string, pos=0):
@@ -154,7 +170,7 @@ class Or(MultiElement):
 		for ee in elements:
 			try:
 				return ee.parseString(string, pos)
-			except ParserException:
+			except ParseException:
 				pass
 		if res:
 			return res
