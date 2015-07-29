bd095ca8f73a975dd3efde5edbf9305412af96b5
Kevin Laeufer <kevin.laeufer@rwth-aachen.de>
2015-07-29 18:29:45 +0200

    [example] agents: start working on a temperature subscriber

---
+++ examples/agents/SimpleTemperatureSubscriber/src/de/rwth_aachen/awap/example/agents/SimpleTemperatureSubscriber.java
@@ -0,0 +1,19 @@
+package de.rwth_aachen.awap.example.agents;
+
+import de.rwth_aachen.awap.Agent;
+import de.rwth_aachen.awap.IDomainFacilitator;
+
+public class SimpleTemperatureSubscriber extends Agent {
+
+	public SimpleTemperatureSubscriber(byte id, IDomainFacilitator df) {
+		super(id, df);
+	}
+
+
+	public void setup() {
+	}
+
+	public void tearDown() {
+
+	}
+}
