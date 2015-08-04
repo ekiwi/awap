46d3c29d83ecc042b3bdd6fb74f01c89abd8445b
Kevin Laeufer <kevin.laeufer@rwth-aachen.de>
2015-08-04 18:15:47 +0200

    [scons] set some special paths

--- scons/awap.py
+++ scons/awap.py
@@ -47,10 +47,19 @@ build
 
 import os
 
+def awap_method(env, configuration):
+	""" This is a pseudo builder that generates a number of targets that
+		can be executed in order to build different parts of awap.
+	"""
+	print("Awap({})".format(configuration))
+
 def generate(env):
 	# define some paths
 	env['AWAP_SCONS_TOOLS'] = os.path.dirname(os.path.abspath(__file__))
-	env['AWAP_ROOT'] = os.path.abspath(os.path.join(env['AWAP_SCONS_TOOLS'], '..'))
+	env['AWAP_ROOT'] = os.path.join(env['AWAP_SCONS_TOOLS'], '..')
+	env['AWAP_LIB_COMMON'] = os.path.join(env['AWAP_ROOT'], 'lib', 'common')
+	env['AWAP_LIB_JADE'] = os.path.join(env['AWAP_ROOT'], 'lib', 'jade')
+	env['AWAP_LIB_NODE'] = os.path.join(env['AWAP_ROOT'], 'lib', 'node')
 
 	# import terminal type to enable gcc/clang to print colored output
 	# http://stackoverflow.com/questions/9922521/why-doesnt-clang-show-color-output-under-scons
@@ -58,6 +67,12 @@ def generate(env):
 
 	# copy path from os environment
 	env['ENV']['PATH'] = os.environ['PATH']
+
+	# determine build directory
+	if not 'AWAP_BUILDPATH' in env:
+		env['AWAP_BUILDPATH'] = os.path.join(env.Dir('.').abspath, 'build')
+	# set ostfriesentee build path relative to awap build path
+	env['OFT_BUILDPATH'] = os.path.join(env['AWAP_BUILDPATH'], 'ostfriesentee')
 
 	# make sure to include all needed external tools
 	env.Tool('default')
@@ -68,6 +83,9 @@ def generate(env):
 
 	# load awap specific tools
 	env.Tool('communication')
+
+	# add pseudo builder to generate targets for a specific awap configuration
+	env.AddMethod(awap_method, 'Awap')
 
 
 def exists(env):
