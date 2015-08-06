febd9bb58a815c8473e810fe9df37eb552ab0f99
Kevin Laeufer <kevin.laeufer@rwth-aachen.de>
2015-08-06 15:07:12 +0200

    [libcommon] RxMessage: does not need a transmission count

--- lib/common/java/de/rwth_aachen/awap/RxMessage.java
+++ lib/common/java/de/rwth_aachen/awap/RxMessage.java
@@ -2,9 +2,4 @@ package de.rwth_aachen.awap;
 
 public abstract class RxMessage {
 	public RemoteAgent sender;
-
-	/**
-	 * Tracks the number of retransmission attempts.
-	 */
-	public int retransmissions = 0;
 }
