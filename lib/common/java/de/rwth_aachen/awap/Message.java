45f4979af2d2b7c1b99e0b4aa8c206a3cf1df92a
Kevin Laeufer <kevin.laeufer@rwth-aachen.de>
2015-08-06 14:42:04 +0200

    [libcommon] generate message classes

--- lib/common/java/de/rwth_aachen/awap/Message.java
+++ lib/common/java/de/rwth_aachen/awap/Message.java
@@ -1,6 +1,11 @@
 package de.rwth_aachen.awap;
 
 public abstract class Message {
-	Agent sender;
-	Agent receiver;
+	public Agent sender;
+	public Agent receiver;
+
+	/**
+	 * Tracks the number of retransmission attempts.
+	 */
+	public int retransmissions = 0;
 }
