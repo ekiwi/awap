0087f05bfc566994797ea0e0c6a7e0bbad0c56a9
Kevin Laeufer <kevin.laeufer@rwth-aachen.de>
2015-08-07 14:09:02 +0200

    [libcommon] generate communication stubs in ServiceClient

---
+++ lib/common/java/de/rwth_aachen/awap/ServiceClient.java
@@ -0,0 +1,12 @@
+package de.rwth_aachen.awap;
+
+public class ServiceClient {
+	protected LocalAgent parent;
+	public ServiceClient(LocalAgent parent) {
+		this.parent = parent;
+	}
+
+	public void registerListener() {
+		// TODO: implement!
+	}
+}
