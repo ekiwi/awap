8332e9503f67bef6be1c664621e229fd7cd4f82a
Kevin Laeufer <kevin.laeufer@rwth-aachen.de>
2015-08-09 16:47:21 +0200

    [tools] entropy: start huffman implementation in python

---
+++ tools/entropy/python/huffman.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+# huffman.py
+# Copyright (c) 2015, Kevin Laeufer <kevin.laeufer@rwth-aachen.de>
+
+"""
+"""
+
+if __name__ == "__main__":
+	print("huffman.py")
