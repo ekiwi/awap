a0e0de887ee28ac85cac39472dc86a0ad14b2b2a
Kevin Laeufer <kevin.laeufer@rwth-aachen.de>
2015-08-09 20:16:20 +0200

    [tools] entropy: add python script to calculate huffman code

--- tools/entropy/python/huffman.py
+++ tools/entropy/python/huffman.py
@@ -7,5 +7,140 @@
 """
 """
 
+import string
+
+class HuffmanLeaf(object):
+	""" Represents a symbol of the source langugage
+	"""
+	def __init__(self, value):
+		self.value = value
+		self.count = 0
+		self.p = 0.0
+		self.code = None
+
+	def propagate_code(self, code):
+		self.code = code
+
+	def __str__(self):
+		if len(self.value) == 1:
+			value = '0x{:02X}'.format(ord(self.value))
+		else:
+			value = self.value
+		return ('"{:<6}": {:4} ({:.2%}) => {}'.format(value, self.count, self.p, self.code))
+
+class HuffmanNode(object):
+	def __init__(self, p0, p1):
+		self.value = None
+		self.count = p0.count + p1.count
+		self.p = p0.p + p1.p
+		if p0.p > p1.p:
+			self.parent_more_likely = p0
+			self.parent_less_likely = p1
+		else:
+			self.parent_more_likely = p1
+			self.parent_less_likely = p0
+
+	def propagate_code(self, code):
+		self.parent_more_likely.propagate_code("0" + code)
+		self.parent_less_likely.propagate_code("1" + code)
+
+
+class HuffmanCode(object):
+	def __init__(self):
+		# will be added to count when calculating distribution
+		self.base_count = 1
+		self.symbols = [HuffmanLeaf(chr(ii)) for ii in range(256)]
+		self.add_symbol('base')
+		self.add_symbol('util')
+		self.add_symbol('Temperature')
+		self.add_symbol('OBJECT')
+		self.add_symbol('awap-common')
+
+
+	def add_symbol(self, symbol):
+		if isinstance(symbol, HuffmanLeaf):
+			self.symbols.append(symbol)
+		else:
+			self.symbols.append(HuffmanLeaf(symbol))
+
+	def count_symbols(self, inp):
+		# sort symbols
+		sorted_symbols = sorted(self.symbols, key=lambda s: -len(s.value))
+
+		# loop over file, trying to find symbols in a greedy manner
+		# NOTE: especially the way we treat the regular bytes as 'symbols'
+		#       makes this not ver efficient
+		#       however: we do not really care, since this will only be done
+		#       once
+		ii = 0
+		length = len(inp)
+		while ii < length:
+			old_ii = ii
+			for symbol in sorted_symbols:
+				if len(symbol.value) <= ii + length:
+					if inp[ii:ii+len(symbol.value)] == symbol.value:
+						symbol.count += 1
+						ii += len(symbol.value)
+						break
+			if ii == old_ii:
+				print("Error: could not match byte 0x{:02X}".format(inp[ii]))
+				exit(1)
+
+	def generate(self):
+		""" Generates a Huffman code based on the symbols collected
+		    by previous calls to count_symbols.
+		"""
+		# update probabilities
+		total_count = sum([s.count for s in self.symbols]) + len(self.symbols) * self.base_count
+		for symbol in self.symbols:
+			symbol.p = float(symbol.count + self.base_count) / float(total_count)
+
+		# create sorted symbol list
+		symbols = sorted(self.symbols, key=lambda s: -s.p)
+
+		while len(symbols) > 1:
+			combined = HuffmanNode(symbols.pop(), symbols.pop())
+			symbols.append(combined)
+			symbols.sort(key=lambda s: -s.p)
+
+		print("Done merging")
+		print("Total count: {}\tTotal p: {}".format(symbols[0].count, symbols[0].p))
+
+		# assign code to all leafs
+		symbols[0].propagate_code("")
+
+	def print_symbols(self):
+		symbols = sorted(self.symbols, key=lambda s: -s.p)
+		print('\n'.join([str(s) for s in symbols]))
+
+	def print_compression_statistics(self, inp):
+		# FIXME: very hacky right now! resets count in symbols...
+		for s in self.symbols: s.count = 0
+		self.count_symbols(inp)
+		old_size = sum([len(s.value) * s.count  for s in self.symbols])
+		new_size = sum([len(s.code) * s.count for s in self.symbols]) / 8.0
+		print("Size: {} => {} ({:.2%})".format(old_size, new_size, new_size / old_size))
+
+
 if __name__ == "__main__":
-	print("huffman.py")
+	import os
+
+	hc = HuffmanCode()
+
+	files = ['../../../examples/simple/build/ostfriesentee/app/TemperatureSensor/TemperatureSensor.di',
+		'../../../examples/simple/build/ostfriesentee/app/SimpleTemperatureSubscriber/SimpleTemperatureSubscriber.di']
+
+	for filename in files: 
+		with open(filename, 'rb') as ff:
+			inp = ff.read()
+		hc.count_symbols(inp)
+
+	hc.generate()
+
+	hc.print_symbols()
+
+	for filename in files: 
+		with open(filename, 'rb') as ff:
+			inp = ff.read()
+		print(os.path.basename(filename))
+		hc.print_compression_statistics(inp)
