effb23f7c83726af03595a0a86c33129c4331867
Kevin Laeufer <kevin.laeufer@rwth-aachen.de>
2015-08-17 17:33:05 +0200

    [huffman] can read encoded file

--- tools/huffman/src/huffman.cpp
+++ tools/huffman/src/huffman.cpp
@@ -1,7 +1,113 @@
 #include <iostream>
 #include <fstream>
+#include <bitset>
+
+#pragma GCC diagnostic ignored "-Wunused-variable"
+
+template<typename T>
+struct Slice
+{
+	Slice(T* ptr, size_t len) : data(ptr), length(len) {}
+
+	T* data;
+	size_t length;
+
+	inline void increment() {
+		++data;
+		--length;
+	}
+};
+
+template<typename T, size_t N>
+inline Slice<T>& slice(T(&ptr)[N])
+{
+	Slice<T> sl(ptr, N);
+	return sl;
+}
+
+/// contains up to 32bit that will be right aligned
+struct CodeBits
+{
+	uint32_t bits;
+	uint32_t count;	// number of bits that are valid
+};
+
+class SymbolSource
+{
+	Slice<uint8_t> source;
+	CodeBits codeBits = { 0, 0 };
+
+public:
+	SymbolSource(Slice<uint8_t>& src) :
+		source(src),
+		codeBits() {
+		updateCodeBits(0);
+	}
+
+	const CodeBits& getCodeBits() {
+		return codeBits;
+	}
+
+	const size_t getRemainingBits() {
+		return source.length * 8 + codeBits.count;
+	}
+
+	size_t updateCodeBits(uint32_t usedBits) {
+		// discard used bits
+		codeBits.bits <<= usedBits;
+		codeBits.count -= usedBits;
+
+		// refill code bits
+		while(codeBits.count <= (32 - 8) && source.length > 0) {
+			uint32_t byte = source.data[0];
+			source.increment();
+			codeBits.bits |= byte << (32 - codeBits.count - 8);
+			codeBits.count += 8;
+		}
+
+		return source.length;
+	}
+};
+
+template<uint32_t Code, uint32_t Bits>
+struct Symbol
+{
+	static constexpr uint32_t ZeroBits = 32 - Bits;
+	static constexpr uint32_t Mask = ((1 << Bits) - 1) << ZeroBits;
+	static constexpr uint32_t Compare = Code << ZeroBits;
+	/// returns how many bits where matched
+	static inline constexpr uint32_t match(const CodeBits input) {
+		// check if enough valid bits to match this symbol
+		if(input.count >= Bits) {
+			if((input.bits & Mask) == Compare) {
+				return Bits;
+			}
+		}
+		return 0;
+	}
+};
 
 #include "code.hpp"
+
+inline static bool decode(Slice<uint8_t> input, Slice<uint8_t> output) {
+	SymbolSource source(input);
+
+	uint32_t bitsMatched = 0;
+	while(bitsMatched = Code::decode_symbol(source.getCodeBits())) {
+		source.updateCodeBits(bitsMatched);
+	}
+
+	if(source.getRemainingBits() == 0) {
+		return true;
+	}
+
+	if((source.getRemainingBits() / 8) == 0 && source.getCodeBits().bits == 0) {
+		// assume trailing zeros
+		return true;
+	}
+
+	return false;
+}
 
 
 
@@ -10,7 +116,7 @@ int main(int argc, char* argv[]) {
 		std::cout << argv[0] << " COMPRESSED [ORIGINAL]" << std::endl;
 		return 1;
 	}
-	std::string compressed(argv[2]);
+	std::string compressed(argv[1]);
 
 	std::ifstream input(compressed, std::ios::in|std::ios::binary|std::ios::ate);
 	if(!input.is_open()) {
@@ -27,13 +133,10 @@ int main(int argc, char* argv[]) {
 
 	std::cout << "Trying to decompress input: " << compressed << std::endl;
 
-	
+	uint8_t* inp = reinterpret_cast<uint8_t*>(content);
+	uint8_t output[size*4];
 
-
-//	const uint8_t* input = reinterpret_cast<uint8_t*>(content);
-//	uint8_t output[size];
-//	Huffman::encode(input, output, size);
-
-//	uint8_t decoded_output[size];
-//	Huffman::decode(output, decoded_output, size);
+	if(!decode(Slice<uint8_t>(inp, size), Slice<uint8_t>(output, size*4))) {
+		std::cout << "Failed to decompress input: " << compressed << std::endl;
+	}
 }
