86e9ef3c7f11dd27d8da512d32c41689d6528deb
Kevin Laeufer <kevin.laeufer@rwth-aachen.de>
2015-09-14 15:22:26 +0200

    [libmote] build native handler for awap-mote

---
+++ lib/mote/c/dummy.c
@@ -0,0 +1,8 @@
+/* This is a dummy source file.
+   It is used in order to make
+   osfriesentee compile the awap-mote native handler
+   without the need to implement any of the native
+   methods in this directory.
+   The actual implementation of the native methods
+   can be found in the cpp source directory.
+*/
