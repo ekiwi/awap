f572a6593c6056f25fca911c4a84241ded83e800
Kevin Laeufer <kevin.laeufer@rwth-aachen.de>
2015-09-16 15:39:26 +0200

    [example] TemperatureInterface: move away from subscriber model

--- example/agents/TemperatureSensor/src/de/rwth_aachen/awap/example/agents/TemperatureSensor.java
+++ example/agents/TemperatureSensor/src/de/rwth_aachen/awap/example/agents/TemperatureSensor.java
@@ -1,14 +1,10 @@
 package de.rwth_aachen.awap.example.agents;
 
-import java.util.ArrayList;
-
 import de.rwth_aachen.awap.Agent;
-import de.rwth_aachen.awap.RemoteAgent;
 import de.rwth_aachen.awap.enums.Building;
 import de.rwth_aachen.awap.enums.Room;
 import de.rwth_aachen.awap.enums.SupplyCircuit;
 import de.rwth_aachen.awap.enums.TemperatureSensorType;
-import de.rwth_aachen.awap.node.AbstractNode;
 
 public class TemperatureSensor extends Agent {
 
@@ -22,20 +18,9 @@ public class TemperatureSensor extends Agent {
 			super(TemperatureSensor.this, Building.Build1, SupplyCircuit.SC1, Room.R1, TemperatureSensorType.Troom);
 		}
 
-		private ArrayList<RemoteAgent> subscribers = new ArrayList<RemoteAgent>();
-
-		public void onReceive(Subscribe msg) {
-			if(!this.subscribers.contains(msg.remoteAgent)) {
-				this.subscribers.add(msg.remoteAgent);
-				// send temperature to new subscriber
-				this.send(new Temperature(msg.remoteAgent, (short)(1337)));
-			}
-		}
-
-		public void onReceive(Unsubscribe msg) {
-			if(this.subscribers.contains(msg.remoteAgent)){
-				this.subscribers.remove(msg.remoteAgent);
-			}
+		public void onReceive(RequestTemperature msg) {
+			// TODO: read temperature from hardware
+			this.send(new Temperature(msg.remoteAgent, (short)(1337)));
 		}
 	}
 }
