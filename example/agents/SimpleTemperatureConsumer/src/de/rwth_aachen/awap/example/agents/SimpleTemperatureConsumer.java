df4b4015187dee67752900d99cc041a7c61c1763
Kevin Laeufer <kevin.laeufer@rwth-aachen.de>
2015-09-16 17:07:04 +0200

    [example] TemperatureConsumer: adjust code to desired interface

--- example/agents/SimpleTemperatureConsumer/src/de/rwth_aachen/awap/example/agents/SimpleTemperatureConsumer.java
+++ example/agents/SimpleTemperatureConsumer/src/de/rwth_aachen/awap/example/agents/SimpleTemperatureConsumer.java
@@ -2,19 +2,29 @@ package de.rwth_aachen.awap.example.agents;
 
 import de.rwth_aachen.awap.Agent;
 import de.rwth_aachen.awap.enums.Building;
+import de.rwth_aachen.awap.enums.Room;
 import de.rwth_aachen.awap.service.remote.ITemperatureServiceClient;
 import de.rwth_aachen.awap.service.remote.TemperatureService;
 import de.rwth_aachen.awap.service.remote.TemperatureService.Temperature;
 
 public class SimpleTemperatureConsumer extends Agent implements ITemperatureServiceClient {
+	private TemperatureService.Type temperaturSensorType;
 
 	public void setup() {
-		TemperatureService.registerListener(this, new TemperatureService.BuildingProperty(Building.Build1));
+		this.requestWakeUp(500);
+		// temperature services in Building1 in Room1 or Room2
+		this.temperaturSensorType = new TemperatureService.Type(
+				this).building(Building.Build1).room(Room.R1, Room.R2);
 	}
 
 	public void onReceive(Temperature msg) {
 		System.out.println("Received Temperature: " + msg.value);
 		System.out.println("From: " + msg.remoteAgent.id);
+	}
+
+	public void onWakeUp() {
+		this.temperaturSensorType.send(new TemperatureService.RequestTemperature());
+		this.requestWakeUp(500);
 	}
 
 }
