ca8a41c8285e7aa10b4f8e80fa1454c93c4862e5
Kevin Laeufer <kevin.laeufer@rwth-aachen.de>
2015-09-17 23:08:18 +0200

    [mote] load consumer agent

--- example/mote/src/main.cpp
+++ example/mote/src/main.cpp
@@ -5,15 +5,15 @@ using namespace awap;
 
 extern unsigned char di_temperature_agent_data[];
 extern size_t di_temperature_agent_size;
-extern unsigned char di_subscriber_agent_data[];
-extern size_t di_subscriber_agent_size;
+extern unsigned char di_consumer_agent_data[];
+extern size_t di_consumer_agent_size;
 
 int main() {
 	std::cout << "Awap Mote Runtime" << std::endl;
 
 	Awap::init(0);
 	Awap::loadAgent(di_temperature_agent_data, di_temperature_agent_size);
-	Awap::loadAgent(di_subscriber_agent_data, di_subscriber_agent_size);
+	Awap::loadAgent(di_consumer_agent_data, di_consumer_agent_size);
 
 	return 0;
 }
