9ce5e1d5b5ba3b9ec5807b70b2eff92ed58957f2
Kevin Laeufer <kevin.laeufer@rwth-aachen.de>
2015-09-23 12:31:55 +0200

    [libmote] DirectoryService: test duplicate entries

--- lib/mote/cpp/test/service_directory_test.cpp
+++ lib/mote/cpp/test/service_directory_test.cpp
@@ -8,14 +8,27 @@ ServiceDirectoryTest::testInsert()
 {
 	using Directory = AgentDirectory<3, 8>;
 
-	Directory dir;
-
 	// test capacity limit
-	Directory::Entry entry { { 0x12345678 }, {1, 2}, 0x22};
-	for(auto ii = 0; ii < 8; ++ii) {
-		dir.insert(entry);
-		entry.serviceType++;
+	{
+		Directory dir;
+		Directory::Entry entry { { 0x12345678 }, {1, 2}, 0x22};
+		for(auto ii = 0; ii < 8; ++ii) {
+			dir.insert(entry);
+			entry.serviceType++;
+		}
+		TEST_ASSERT_EQUALS(dir.count(), 8u);
+		TEST_ASSERT_FALSE(dir.insert(entry));
+		TEST_ASSERT_EQUALS(dir.count(), 8u);
 	}
-	TEST_ASSERT_FALSE(dir.insert(entry));
+
+	// make sure duplicate entries do not take up space
+	{
+		Directory dir;
+		Directory::Entry entry { { 0x12345678 }, {1, 2}, 0x22};
+		for(auto ii = 0; ii < 8; ++ii) {
+			dir.insert(entry);
+		}
+		TEST_ASSERT_EQUALS(dir.count(), 1u);
+	}
 
 }
