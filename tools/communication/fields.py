3d5dcb88c67000d1ae9d016221c37aecb59add37
Kevin Laeufer <kevin.laeufer@rwth-aachen.de>
2015-10-05 12:25:06 +0200

    [tools] communication: fields: add support for bits > 8

--- tools/communication/fields.py
+++ tools/communication/fields.py
@@ -4,6 +4,7 @@
 # fiels.py
 # Copyright (c) 2015, Kevin Laeufer <kevin.laeufer@rwth-aachen.de>
 
+import math
 
 class Field(object):
 	def __init__(self, name, bits):
@@ -20,17 +21,24 @@ class Byte(object):
 	def __init__(self, field=None):
 		self.free_bits = 8
 		self.fields = []
+		self.bytes = 0	#additional bytes to hold fields with bits > 8
 		if isinstance(field, Field):
 			self.insert(field)
 
 
 	def insert(self, field):
-		if(field.bits > self.free_bits):
+		additional_bytes_needed = int(math.ceil(field.bits / 8.0)) - 1
+		bits_needed = field.bits - (additional_bytes_needed * 8)
+		if(bits_needed > self.free_bits):
 			return False
-		else:
-			self.free_bits -= field.bits
-			self.fields.append(field)
-			return True
+		if additional_bytes_needed > 0:
+			if self.bytes > 0:
+				return False
+			else:
+				self.bytes = additional_bytes_needed
+		self.free_bits -= bits_needed
+		self.fields.append(field)
+		return True
 
 def sort(fields):
 	print(fields)
@@ -64,11 +72,14 @@ if __name__ == "__main__":
 	fields.append(Field("j", 5))
 	fields.append(Field("k", 5))
 	fields.append(Field("l", 2))
+	fields.append(Field("m", 12))
+	fields.append(Field("n", 17))
 	fields.append(Field("b3", 1))
 
 	bytes = sort(fields)
+	byte_count = sum((1 + b.bytes) for b in bytes)
 
 	bits = sum(f.bits for f in fields)
 
 	print("Could fit {} bits ({} bytes) into {} bytes => {}%.".format(
-		bits, bits / 8.0, len(bytes), int(len(bytes) / (bits / 8.0) * 100)))
+		bits, bits / 8.0, byte_count, int(byte_count / (bits / 8.0) * 100)))
