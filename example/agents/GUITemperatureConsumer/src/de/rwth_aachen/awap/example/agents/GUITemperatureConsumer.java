09622d2df049b57eef70d1d8cdcf52dc50954768
Kevin Laeufer <kevin.laeufer@rwth-aachen.de>
2016-01-28 11:03:53 +0100

    [examples] add jade demo files

---
+++ example/agents/GUITemperatureConsumer/src/de/rwth_aachen/awap/example/agents/GUITemperatureConsumer.java
@@ -0,0 +1,41 @@
+/**
+ * GUITemperatureConsumer.java
+ *
+ * Copyright (c) 2016 Kevin Laeufer <kevin.laeufer@rwth-aachen.de>
+ *
+ * This file is part of awap.
+ */
+
+package de.rwth_aachen.awap.example.agents;
+
+import de.rwth_aachen.awap.Agent;
+import de.rwth_aachen.awap.enums.Building;
+import de.rwth_aachen.awap.enums.Room;
+import de.rwth_aachen.awap.messages.TemperatureService.RequestTemperature;
+import de.rwth_aachen.awap.messages.TemperatureService.Temperature;
+import de.rwth_aachen.awap.service.remote.ITemperatureServiceClient;
+import de.rwth_aachen.awap.service.remote.TemperatureService;
+
+public class GUITemperatureConsumer extends Agent implements ITemperatureServiceClient {
+	private TemperatureService.Type temperaturSensorType;
+
+	public void setup() {
+		this.requestWakeUp(500, new Integer(0));
+		// temperature services in Building1 in Room1 or Room2
+		this.temperaturSensorType = new TemperatureService.Type(
+				this).building(Building.Build1).room(Room.R1);
+	}
+
+	public void onReceive(Temperature msg) {
+		System.out.println("Received Temperature: " + msg.value);
+		System.out.println("From: " + msg.getRemoteAgent().id);
+	}
+
+	public void onWakeUp(Object obj) {
+		int ii = (Integer)obj;
+		System.out.println("Agent: Woke up: " + ii);
+		this.temperaturSensorType.send(new RequestTemperature());
+		this.requestWakeUp(1000, new Integer(ii+1));
+	}
+
+}
---
+++ example/demo_jade/src/de/rwth_aachen/awap/example/simple/Main.java
@@ -0,0 +1,57 @@
+/**
+ * Main.java
+ *
+ * Copyright (c) 2015 Kevin Laeufer <kevin.laeufer@rwth-aachen.de>
+ *
+ * This file is part of awap.
+ */
+
+
+package de.rwth_aachen.awap.example.simple;
+
+import de.rwth_aachen.awap.jade.node.Node;
+import jade.core.Profile;
+import jade.core.ProfileImpl;
+import jade.core.Runtime;
+import jade.util.ExtendedProperties;
+import jade.util.leap.Properties;
+import jade.wrapper.AgentController;
+import jade.wrapper.ContainerController;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		// Get a hold on JADE runtime
+		final Runtime rt = Runtime.instance();
+
+		// Shut JVM down, when agent runtime closes
+		rt.setCloseVM(true);
+
+		// activate RMA GUI
+		Properties prop = new ExtendedProperties();
+		prop.setProperty(Profile.GUI, "false");
+
+		// Create a default profile
+		Profile p = new ProfileImpl(prop);
+
+		// Create a new main container with profile
+		final ContainerController cc = rt.createMainContainer(p);
+
+		// create temperature node and agent
+		Node temperatureNode= new Node("TemperatureNode0");
+		Node subscriberNode= new Node("ConsumerNode0");
+
+		AgentController temperatureAgent0 = temperatureNode.createNewAgent(cc, "TemperatureAgent0", "de.rwth_aachen.awap.example.agents.TemperatureSensor");
+		temperatureAgent0.start();
+		AgentController temperatureConsumer0 = subscriberNode.createNewAgent(cc, "GUITemperatureConsumer0", "de.rwth_aachen.awap.example.agents.GUITemperatureConsumer");
+		temperatureConsumer0.start();
+
+
+		//Thread.sleep(2000);
+		//temperatureAgent0.kill();
+
+		//Thread.sleep(2000);
+		//temperatureConsumer0.kill();
+
+	}
+}
